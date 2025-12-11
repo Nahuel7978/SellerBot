499c313e0db2de5588163e1fc7bb1140faa1203a
Nahuel7978 <roman.n7978@gmail.com>
2025-12-11 11:45:09 -0300

    Modelos de datos

---
+++ SellerApi/Model/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+# Modelo para cuando agregamos/editamos un ítem en el carrito
+class CartItemRequest(BaseModel):
+    product_id: int
+    qty: int # Si es positivo agrega/actualiza, si es 0 o negativo podría descartar
+
+# Modelo para la actualización del carrito (Lista de ítems)
+class CartUpdate(BaseModel):
+    items: List[CartItemRequest]
