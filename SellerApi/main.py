70ddec755d36bf7ae14215653060d15a198120da
Nahuel7978 <roman.n7978@gmail.com>
2025-12-10 21:13:47 -0300

    archivo principal para la ejecucion de la API

---
+++ SellerApi/main.py
@@ -0,0 +1,62 @@
+# app/main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from Controllers.controller_api import router as api_router
+import logging
+import os
+
+logger = logging.getLogger("scapi")
+scheduler = None
+job_cleaner = None
+
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+APP_PORT = int(os.getenv("PORT"))
+
+# Eventos (startup/shutdown)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Código de inicialización
+    logger.info("API arrancando: inicializando recursos...")
+    try:
+        logger.info("✅ Inicialización completada")
+        
+    except Exception as e:
+        logger.error(f"❌ Error durante inicialización: {e}")
+        raise
+    
+    yield
+    # Código de limpieza
+    logger.info("API apagándose: cerrando recursos...")
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="Seller API",
+        version="0.1.0",
+        description="API para vender productos vía LLM",
+        lifespan=lifespan
+    )
+
+    # Middlewares: CORS ejemplo (ajustar CORS_ORIGINS en config)
+    
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    
+    # Routers
+    app.include_router(api_router, prefix="/SellerAPI/v1")
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    import uvicorn
+    # En Railway, la variable de entorno 'PORT' será asignada por ellos
+    # Usamos 8000 por defecto en local    
+    uvicorn.run(app, host="0.0.0.0", port=APP_PORT)
